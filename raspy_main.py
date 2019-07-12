87a80fff1476f4ef326eea23c98ad52b7f5056a2
chan-shizu <43442412+chan-shizu@users.noreply.github.com>
2019-07-12 14:20:37 +0900

    Add files via upload

--- raspy_main.py
+++ raspy_main.py
@@ -1,36 +1,41 @@
-import gokko, riddle, yomikikase, line_camera, arduino, camera_csv
-import serial
+#import gokko, riddle, yomikikase, line_camera, arduino, camera_csv
+#import serial
 import time, os, random
 
-os.system('sudo chnod 777 /dev/ttyAM0')
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
-flag = 'none'
+#os.system('sudo chnod 777 /dev/ttyAM0')
+#ser = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
+#flag = 'none'
 
-def main():
-    camera_csv.csv_setup()
-    flag = 'none'
-    while (flag != 'set_up'):
-        flag = ser.readline()
-        time.sleep(3)
+def main(number):
+    #camera_csv.csv_setup()
+    #flag = 'none'
+    #while (flag != 'set_up'):
+        #flag = ser.readline()
+        #time.sleep(3)
 
-h = random.randint(0, 6)
-if (h == 0):
-    gokko.main()
+    if (number == 0):
+        #arduino.gameA  
+        print("遊びは0")
 
-elif (h == 1):
-    riddle.main()
+    elif (number == 1):
+        #arduino.gameB 
+        print("遊びは1")
 
-elif (h == 2):
-    yomikikase.main()
+    elif (number == 2):
+        #arduino.gameC 
+        print("遊びは2")
 
-elif (h == 3):
-    arduino.gameA()  
+    elif (number == 3):
+        #arduino.gameD() #gokko 
+        print("遊びは3")
 
-elif (h == 4):
-    arduino.gameB()
+    elif (number == 4):
+        #arduino.gameE() #riddle
+        print("遊びは4")
 
-elif (h == 5):
-    arduino.gameC()   
+    elif (number == 5):
+        #arduino.gameF() #yomikikase  
+        print("遊びは5")
 
 if __name__ == "__main__":
     main()
