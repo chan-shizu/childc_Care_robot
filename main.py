776be259d22323e2569daf5a65db54bce4484064
chan-shizu <bubbler018@gmail.com>
2019-07-26 14:06:28 +0900

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import raspy_main, csv_matomeru, assesmenting_face_expression
 
 while True:
     #line_camera_faceAPI.main()
-    csv_matomeru.main() #csvのデータを整理する
+    csv_matomeru.main() #csvのデータを整理する!
     raspy_main.main(assesmenting_face_expression.main())
 
 
