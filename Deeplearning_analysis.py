54ca891129f9845363e7a30addaac01542427090
chan-shizu <bubbler018@gmail.com>
2019-09-25 15:08:15 +0900

    特に意味はない

---
+++ Deeplearning_analysis.py
@@ -0,0 +1,152 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.externals import joblib
+import csv
+import matplotlib.pyplot as plt
+import line_camera_faceAPI
+import pandas as pds
+from keras.models import Sequential
+from keras.layers import Input, Dense, Dropout, BatchNormalization
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
+
+print("start")
+
+# for note pc(dynabook)
+'''
+path1 =   
+path2 = 
+path3 = 
+'''
+# for desktop pc(university)
+path1 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\過去論文遊びの好み.csv"    
+path2 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\0511camera_matome.csv"
+path3 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\deeplearning_analysis.csv"
+#path4 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\clf_data.pkl"
+path5 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\deeplearning_analysis_result.csv"
+
+def reg_model():
+    model = Sequential()
+    model.add(Dense(10, input_dim=5, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(256, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(128, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(64, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(1))
+
+    #compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    return model
+
+def main():
+    analysis_score = []
+    with open(path3, "w", newline="") as f:
+        writer = csv.writer(f)
+        for i in [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]:
+            training_personality = np.loadtxt(path1,\
+                            delimiter=",",\
+                            skiprows=1,\
+                            max_rows=12,\
+                            usecols=(i),\
+                            encoding="utf-8"\
+                            )
+
+            training_preference = np.loadtxt(path1,\
+                            delimiter=",",\
+                            skiprows=1,\
+                            max_rows=12,\
+                            usecols=(1, 2, 3, 4, 5),\
+                            encoding="utf-8"\
+                            )
+
+            test_preference = np.empty((1, 5))
+            test_preference[0] = np.loadtxt(path2,
+                            delimiter=",",\
+                            skiprows=0,\
+                            max_rows=1,\
+                            usecols=(1, 2, 3, 4, 5),\
+                            encoding="utf-8"\
+                            )
+            np.reshape(test_preference, (1, 5))
+
+            seed = len(training_personality)
+            np.random.seed(seed)
+            model = KerasRegressor(build_fn=reg_model, epochs=100, batch_size=10, verbose=0)
+            model.fit(training_preference, training_personality)
+            #kfold = KFold(n_splits=10, random_state=seed)
+
+            #調整されたパラメータを見る
+            #print(i)
+            #print("model.coef_")
+            #print(model.coef_)
+            #print("model.intercept_")
+            #print(model.intercept_)
+            print("score(0.5以上が目安)")
+            print(model.score(training_preference, training_personality))
+
+            #予測
+            print(model.predict(test_preference))
+            print(model.score(training_preference, training_personality))
+            #np.savetxt(path3,model.score(training_preference, training_personality))
+            analysis_score.append(model.score(training_preference, training_personality))
+        
+            path4 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\deeplearning_clf_data{}.pkl".format(i)
+            joblib.dump(model, path4) #学習データを保存
+
+        writer.writerow(analysis_score)
+
+
+    print(analysis_score)
+
+def fit_analysis():
+    analysis_score = []
+    personality_number = []
+    for i in [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]:
+        df = np.loadtxt(path2,\
+                        delimiter=",",\
+                        skiprows=0,\
+                        max_rows=1,\
+                        usecols=(1, 2, 3, 4, 5),\
+                        encoding="utf-8"\
+                        )
+        print(df)
+        df = df.reshape(1,-1)
+        print(df)
+        path4 = "C:\\Users\\bubbl\\OneDrive\\Desktop\\facial_expression_data\\deeplearning_clf_data{}.pkl".format(i)
+        model = joblib.load(path4) #過去の学習データ(遊びの好みと性格の関係)をロードする
+        predict = model.predict(df) #学習データから数値を予測する
+        print(predict)
+        analysis_score.append(predict)
+        name = "p{}".format(i-6)
+        personality_number.append(name)
+
+    with open(path5, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(personality_number)
+        writer.writerow(analysis_score)
+    
+    plt.bar(personality_number, analysis_score, tick_label=personality_number, align="center") #横軸, 縦軸の配列を指定, 
+    plt.title("child's personality")
+    plt.xlabel("p") #x軸のラベル
+    plt.ylabel("figures") #y軸のラベル
+    plt.grid(True) #格子表示
+    #plt.show() #画像表示
+    plt.savefig("C:\\Users\\bubbl\\OneDrive\\Desktop\\deeplearning_chart.jpg") #画像保存
+    line_camera_faceAPI.line_photo_picture("C:\\Users\\bubbl\\OneDrive\\Desktop\\deeplearning_chart.jpg") #ラインにグラフの画像を送信
+
+if __name__ == "__main__":
+    main()
+    fit_analysis()
